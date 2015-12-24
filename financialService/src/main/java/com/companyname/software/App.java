fa494e69354b149c4306bb9f9e193f162d5f3519
scmgalaxy <rajesh@scmgalaxy.com>
2015-12-24 18:20:49 +0530

    MYD-2
    MYD-2

--- financialService/src/main/java/com/companyname/software/App.java
+++ financialService/src/main/java/com/companyname/software/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Worddld!" );
+        System.out.println( "Helldddo Worddld!" );
     }
 }
